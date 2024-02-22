80d826277283c0718fb784cf34ebe792046797bc
Tobias Lutz <147028185+to-lutz@users.noreply.github.com>
2024-02-22 20:53:49 +0000

    Add API Endpoint for chips

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var chipsRouter = require('./routes/api/chips')
 
 var app = express();
 
@@ -16,6 +17,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../client')));
 
 app.use('/', indexRouter);
+app.use('/api/chips', chipsRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ server/routes/api/chips.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+const data = require('../../data/api/chips.json')
+
+/* GET chips json. */
+router.get('/', function(req, res, next) {
+    res.header("Content-Type",'application/json');
+    res.send(JSON.stringify(data));
+});
+
+module.exports = router;
